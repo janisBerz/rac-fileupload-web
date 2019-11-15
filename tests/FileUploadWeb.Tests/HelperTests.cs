b26bc1f6fc285fd6572be97cfbb89504099c7466
Jānis Bērziņš <janis.berzins@visma.com>
2019-11-15 10:37:27 +0200

    Adding unit test

--- tests/FileUploadWeb.Tests/HelperTests.cs
+++ tests/FileUploadWeb.Tests/HelperTests.cs
@@ -6,17 +6,17 @@ namespace FileUploadWeb.Tests
 {
     public class Helper
     {
-        // [Fact]
-        // public void SetLocalPathToTempFolder()
-        // {
-        //     var result = FileHelpers.SetLocalPath("SomeFileName.zip");
-        //     Assert.NotNull(result);
-        // }
-        // [Fact]
-        // public void SetLocalPathToTempFolder2()
-        // {
-        //     var result = FileHelpers.SetLocalPath2("SomeFileName.zip");
-        //     Assert.NotNull(result);
-        // }
+        [Fact]
+        public void SetLocalPathToTempFolder()
+        {
+            var result = FileHelpers.SetLocalPath("SomeFileName.zip");
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void SetLocalPathToTempFolder2()
+        {
+            var result = FileHelpers.SetLocalPath2("SomeFileName.zip");
+            Assert.NotNull(result);
+        }
     }
 }
