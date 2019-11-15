f1231de6c97a6b1adf0393dba53ee90d66a089f5
Jānis Bērziņš <janis.berzins@visma.com>
2019-11-15 10:37:49 +0200

    Removed unit test

--- tests/FileUploadWeb.Tests/HelperTests.cs
+++ tests/FileUploadWeb.Tests/HelperTests.cs
@@ -12,11 +12,11 @@ namespace FileUploadWeb.Tests
             var result = FileHelpers.SetLocalPath("SomeFileName.zip");
             Assert.NotNull(result);
         }
-        [Fact]
-        public void SetLocalPathToTempFolder2()
-        {
-            var result = FileHelpers.SetLocalPath2("SomeFileName.zip");
-            Assert.NotNull(result);
-        }
+        // [Fact]
+        // public void SetLocalPathToTempFolder2()
+        // {
+        //     var result = FileHelpers.SetLocalPath2("SomeFileName.zip");
+        //     Assert.NotNull(result);
+        // }
     }
 }
